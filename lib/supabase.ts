94bf19f450502344fa7ec2f1d81a7b146eb9448d
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2025-02-19 20:12:50 +0530

    google oauth added

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jmfbclnegkmhfypaqjbf.supabase.co';
+const supabaseUrl = 'https://houmlxwheveremcucbve.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZmJjbG5lZ2ttaGZ5cGFxamJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5ODYzMzgsImV4cCI6MjAyNTU2MjMzOH0.ZmCN8xTjnr38F73CQZdSP7TT98WFQPlJ85PMoAaVwvQ';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvdW1seHdoZXZlcmVtY3VjYnZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk5NzUwMDUsImV4cCI6MjA1NTU1MTAwNX0.Q5GGblWYVEJKo-XYWdsodtRiau1Gx7KDLl_O2_XBit8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
